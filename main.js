c05049e567cd8dd7e2bade60f70379354ab84527
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-05-29 19:07:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+//🚀 Day 7 Challenge: Start Coding! 🚀
+Object.defineProperty(exports, "__esModule", { value: true });
+//Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print 
+//a message indicating the number of people you are inviting to dinner.
+let guests = ["Abdullah", "Hina Naseer", "Saad", "Miss Hina", "Muhammad Saad", "Khizar",];
+console.log(`I am inviting ${guests.length} people to dinner.`);
+//Question 20: Think of something you could store in an array. For example, you could make a list of
+// mountains, rivers, countries, cities, languages, or anything else you’d like.
+// Write a program that creates a list containing these items.
+let countries = ["Japan", "Canada", "New Zealand", "Saudia Arabia", "Palestine"];
+console.log("Countries I'd like to visit:", countries);
+//Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects
+// containing these items.
+let book = {
+    title: "20 Stories of Corona Survivors",
+    author: "Muhammad Naeem",
+    yearPublished: 2021
+};
+console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}`);
